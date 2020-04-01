1613dcc81152cebb64af83f9de33afa8de774b71
MixusMinimax <maxi.barmetler@gmail.com>
2020-04-01 12:22:50 +0200

    parse path

--- maze.py
+++ maze.py
@@ -1,5 +1,23 @@
+import os
+
+
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error(f"The file {arg} does not exist!")
+    else:
+        return arg  # return path
+
+
 def main():
-    print("Hello!")
+    # Parse arguments
+    import argparse
+    parser = argparse.ArgumentParser(description="Generate and display mazes")
+    parser.add_argument('-p', '--path', action='store',
+                        help="file that contains maze or that maze will be writte to", metavar="FILE",
+                        type=lambda x: is_valid_file(parser, x))
+    
+    args = parser.parse_args()
+    print(args)
 
 
 if __name__ == "__main__":
